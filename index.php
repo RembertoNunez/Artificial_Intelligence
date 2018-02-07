7af412022ad6fc65d324b937b5873249977b00eb
Remberto Nunez <remnunez@csumb.edu>
2018-02-07 18:14:24 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta charset=utf-8 />
+        <meta name="description" content="description">
+        <title>AI: Artificial Intelligence</title>
+        <link rel="stylesheet" media="screen" href="css/style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lekton" rel="stylesheet">
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+        <h1>
+            AI: Artificial Intelligence
+        </h1>
+        <ul id="nav">
+        	<li class="left"><a href="index.php">About Artificial Intelligence</a></li>
+        	<li class="right"><a href="deeplearning.html">Deep Learning</a></li>
+        	<li class="left"><a href="machinelearning.html">Machine Learning</a></li>
+        	<li class="right"><a href="naturalprocessing.html">Natural Language Processing</a></li>
+        </ul>
+        </header>
+    </body>
+    <main>
+        <div id="aboutAI">
+            <p>Iasd;lfkjasd;lfkja;lsdkjf;alskdjfa;lsdkjfad;lskfjal;ksdjf</p>
+        </div>
+    </main>
+    <footer>
+        <hr>
+            Internet Programming CST-336. 2018&copy; Nunez <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+        <figure id="logo">
+                <img src="img/csumb.jpg" alt="CSUMB Logo" />
+        </figure>
+    </footer>
+</html>
