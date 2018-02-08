a54624c9fa9ac035ea413a19fb105398860a46a0
Remberto Nunez <remnunez@csumb.edu>
2018-02-08 06:23:23 +0000

    second commit

--- index.php
+++ index.php
@@ -29,30 +29,31 @@
         	<li class="right"><a href="aiin.html">AI IN</a></li>
         </ul>
         </header>
-    <section id = "left">
-        <figure>
-            <img src="img/ai.jpg" alt="brain" />
-        </figure>
-    </section>
-    <section id ="right">
-        <h2>What is AI?</h2>
-        <p>AI or artificial intelligence is the simulation of human intelligence processes by machines,
-        especially computer systems. These processes include learning, reasoning, and self-correction. It also includes 
-        speech recognition and machine vision.</p>
-        <p>The idea of an AI was brought by John McCarthy, a computer scientist, in 1956 at The Dartmouth Conference. 
-        It has gained prominence recently due, in part, to big data, or the increase in speed, size and variety of data businesses 
-        are now collecting. AI can perform tasks such as identifying patterns in the data more efficiently than humans, 
-        enabling businesses to gain more insight out of their data.</p>
-        <p><a href="https://www.investopedia.com/terms/a/artificial-intelligence-ai.asp">AI Link</a></p>
-    </section>
-    <footer>
-        <hr>
-            Internet Programming CST-336. 2018&copy; Nunez <br />
-            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
-            It is used for academic purposes only.
-        <figure id="logo">
-                <img src="img/csumb.jpg" alt="CSUMB Logo" />
-        </figure>
-    </footer>
+        <section id = "left">
+            <h2>HI</h2>
+            <figure>
+                <img src="img/ai.jpg" alt="brain" />
+            </figure>
+        </section>
+        <section id ="right">
+            <h2>What is AI?</h2>
+            <p>AI or artificial intelligence is the simulation of human intelligence processes by machines,
+            especially computer systems. These processes include learning, reasoning, and self-correction. It also includes 
+            speech recognition and machine vision.</p>
+            <p>The idea of an AI was brought by John McCarthy, a computer scientist, in 1956 at The Dartmouth Conference. 
+            It has gained prominence recently due, in part, to big data, or the increase in speed, size and variety of data businesses 
+            are now collecting. AI can perform tasks such as identifying patterns in the data more efficiently than humans, 
+            enabling businesses to gain more insight out of their data.</p>
+            <p><a href="https://www.investopedia.com/terms/a/artificial-intelligence-ai.asp">AI Link</a></p>
+        </section>
+        <footer>
+            <hr>
+                Internet Programming CST-336. 2018&copy; Nunez <br />
+                <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+                It is used for academic purposes only.
+            <figure id="logo">
+                    <img src="img/csumb.jpg" alt="CSUMB Logo" />
+            </figure>
+        </footer>
     </body>
 </html>
